9bb3e47aae91d6f7ee18c2f58ff4dc3554b2653d
Ajisha <ajisha673@gmail.com>
2026-09-19 00:35:53 +0530

    Add Session 2 String problems

---
+++ src/main/java/string/assigment_problems/WordReversalEncoder.java
@@ -0,0 +1,23 @@
+public class WordReversalEncoder {
+
+    static String reverseEachWord(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            StringBuilder reversed = new StringBuilder();
+
+            for (int i = word.length() - 1; i >= 0; i--) {
+                reversed.append(word.charAt(i));
+            }
+
+            result.append(reversed).append(" ");
+        }
+
+        return result.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseEachWord("hello club"));
+    }
+}
