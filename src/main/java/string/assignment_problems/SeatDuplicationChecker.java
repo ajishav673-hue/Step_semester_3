19e8e2ea5b26bbd29f5fec7f5d4d639d15ab28f6
Ajisha <ajisha673@gmail.com>
2026-09-18 23:05:42 +0530

    Add Session 1 String problems

---
+++ src/main/java/string/assignment_problems/SeatDuplicationChecker.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class SeatDuplicationChecker {
+
+    static void checkDuplicateSeats(int[] seatNumbers) {
+        boolean found = false;
+
+        for (int i = 0; i < seatNumbers.length; i++) {
+            boolean alreadyPrinted = false;
+
+            for (int k = 0; k < i; k++) {
+                if (seatNumbers[i] == seatNumbers[k]) {
+                    alreadyPrinted = true;
+                    break;
+                }
+            }
+
+            if (!alreadyPrinted) {
+                for (int j = i + 1; j < seatNumbers.length; j++) {
+                    if (seatNumbers[i] == seatNumbers[j]) {
+                        System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                        found = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of seats: ");
+        int n = sc.nextInt();
+
+        int[] seats = new int[n];
+
+        System.out.println("Enter seat numbers:");
+        for (int i = 0; i < n; i++) {
+            seats[i] = sc.nextInt();
+        }
+
+        checkDuplicateSeats(seats);
+
+        sc.close();
+    }
+}
